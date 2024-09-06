88f9f58976c6a48978345fdfbcd77fdde90b6a5e
Jason Pauley <jasonpauley@gmail.com>
2024-09-06 10:26:34 -0700

    Update exploratory_reseller.sql

--- Data_Exploratory/exploratory_reseller.sql
+++ Data_Exploratory/exploratory_reseller.sql
@@ -11,9 +11,43 @@ countryregion
 
 SELECT *
 FROM reseller
-limit 5;
+LIMIT 5;
+
 277	Specialty Bike Shop	  The Bicycle Accessories Company	Alhambra	    California	United States
 455	Value Added Reseller	Timely Shipping Service	        Alpine	      California	United States
 609	Value Added Reseller	Good Toys	                      Auburn	      California	United States
 492	Specialty Bike Shop	  Basic Sports Equipment	        Baldwin Park	California	United States
 365	Specialty Bike Shop	  Distinctive Store	              Barstow	      California	United States
+
+SELECT 
+	DISTINCT stateprovince
+FROM 
+	reseller
+WHERE countryregion = 'United States'
+ORDER BY stateprovince
+LIMIT 10;
+Alabama
+Arizona
+California
+Colorado
+Connecticut
+Florida
+Georgia
+Idaho
+Illinois
+Indiana
+
+SELECT DISTINCT countryregion 
+FROM reseller
+ORDER BY countryregion;
+Australia
+Canada
+France
+Germany
+United Kingdom
+United States
+
+SELECT count(reseller) 
+FROM reseller
+WHERE stateprovince = 'California';
+78
