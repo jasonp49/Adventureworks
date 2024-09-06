27157e25865e9e05e5a07be670f4d87d3f879238
Jason Pauley <jasonpauley@gmail.com>
2024-09-06 10:08:26 -0700

    Create exploratory_region.sql

---
+++ Data_Exploratory/exploratory_region.sql
@@ -0,0 +1,21 @@
+SELECT *
+  FROM information_schema.columns
+ WHERE table_schema = 'public'
+   AND table_name   = 'region';
+salesterritorykey
+region
+country
+Group
+
+SELECT *
+FROM region;
+1	Northwest	      United States	  North America
+2	Northeast	      United States	  North America
+3	Central	        United States	  North America
+4	Southwest	      United States	  North America
+5	Southeast	      United States	  North America
+6	Canada	        Canada	        North America
+7	France	        France	        Europe
+8	Germany	        Germany	        Europe
+9	Australia	      Australia	      Pacific
+10United Kingdom	United Kingdom	Europe
