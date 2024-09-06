0e7b4a93643d709936a6232b5400d6c76277e7bd
Jason Pauley <jasonpauley@gmail.com>
2024-09-06 10:02:41 -0700

    Update exploratory_salesperson.sql

--- Data_Exploratory/exploratory_salesperson.sql
+++ Data_Exploratory/exploratory_salesperson.sql
@@ -11,8 +11,8 @@ upn
 SELECT *
 FROM salesperson
 limit 5;
-272	502097814	Stephen Jiang	North American Sales Manager	stephen-jiang@adventureworks.com
-277	112432117	Brian Welcker	Director of Sales	brian-welcker@adventureworks.com
-281	841560125	Michael Blythe	Sales Representative	michael-blythe@adventureworks.com
-282	191644724	Linda Mitchell	Sales Representative	linda-mitchell@adventureworks.com
-283	615389812	Jillian Carson	Sales Representative	jillian-carson@adventureworks.com
+272	502097814	Stephen Jiang	  North American Sales Manager	stephen-jiang@adventureworks.com
+277	112432117	Brian Welcker	  Director of Sales	            brian-welcker@adventureworks.com
+281	841560125	Michael Blythe	Sales Representative	        michael-blythe@adventureworks.com
+282	191644724	Linda Mitchell	Sales Representative	        linda-mitchell@adventureworks.com
+283	615389812	Jillian Carson	Sales Representative	        jillian-carson@adventureworks.com
