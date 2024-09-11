f9b161b36e62c6d3e06d1ee2401e7cc749078939
Jason Pauley <jasonpauley@gmail.com>
2024-09-11 15:24:00 -0700

    Update exploratory_region.sql

--- Data_Exploratory/exploratory_region.sql
+++ Data_Exploratory/exploratory_region.sql
@@ -20,3 +20,35 @@ FROM region;
 9	Australia	      Australia	      Pacific
 10United Kingdom	United Kingdom	Europe
 
+SELECT r.region, sum(s.sales) 
+FROM sales s
+JOIN region r ON r.salesterritorykey = s.salesterritorykey 
+GROUP BY 1
+ORDER BY 2 DESC 
+Southwest	     17993464
+Canada	       13868976
+Northwest	     12000314
+Southeast	      7635082
+Central	        7630024
+Northeast	      6711984
+France	        4525777
+United Kingdom	3881093
+Germany	        1876784
+Australia	      1390206
+
+SELECT r.region, sum(s.sales) AS August2019Sales
+FROM sales s
+JOIN region r ON r.salesterritorykey = s.salesterritorykey 
+WHERE s.orderdate LIKE '%Aug%2019%'
+GROUP BY 1
+ORDER BY 2 DESC 
+Northwest	      743082
+France	        728611
+Southwest	      673150
+Canada	        575143
+Central	        383240
+Northeast	      288552
+Southeast	      279168
+Germany	        263657
+United Kingdom	223749
+Australia	      145934
